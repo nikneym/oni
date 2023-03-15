526c95bf277911f4f959e3fbe5684fe23ebe7c78
nikneym <halildrk@gmail.com>
2023-03-15 15:10:50 +0300

    oni.wait: Deinitialize `xev.Timer` when event is fired

--- src/oni.zig
+++ src/oni.zig
@@ -5,10 +5,12 @@ const global = @import("global.zig");
 const Lua = lua.Lua;
 const LuaState = lua.LuaState;
 
+// Schedules a new timer event
 pub fn scheduleTimer(co: *Lua, ms: usize) !c_int {
     const timer = try xev.Timer.init();
     const c = try global.getCompletion();
 
+    // schedule a timer
     timer.run(
         global.defaultLoop(),
         c,
@@ -18,10 +20,13 @@ pub fn scheduleTimer(co: *Lua, ms: usize) !c_int {
         onTimerFireDefaultFn,
     );
 
-    return co.yield(0);
+    // push the timer to onFireFn
+    const ptr = co.newUserdata(*const xev.Timer);
+    ptr.* = &timer;
+    return co.yield(1);
 }
 
-// this one just resumes the thread back
+// this timerFn just resumes the thread back
 fn onTimerFireDefaultFn(
     state: ?*LuaState,
     _: *xev.Loop,
@@ -30,9 +35,14 @@ fn onTimerFireDefaultFn(
 ) xev.CallbackAction {
     global.destroyCompletion(c);
     var co = Lua{ .state = state.? };
+    const timer = co.toUserdata(*xev.Timer, 1) catch @panic("unable to receive timer from event");
+    timer.*.deinit();
 
-    res catch {
-        @panic("timer error");
+    // TODO: proper error
+    res catch |e| {
+        co.pushString(@errorName(e));
+        _ = co.resumeThread(1) catch co.raiseError();
+        return .disarm;
     };
 
     // give control back to coroutine
